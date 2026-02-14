4378da4305686e3c87b4b5ab8aa71951e36cb0c0
Santiago Ruiz <santiago.ruiz.v@outlook.com>
2026-02-14 18:27:20 -0500

    Landing Firma 7 V5.4

--- components/ProfilingForm.tsx
+++ components/ProfilingForm.tsx
@@ -23,30 +23,28 @@ export const ProfilingForm: React.FC = () => {
         </div>
 
         <div className="w-full bg-white shadow-2xl border border-gray-200 rounded-xl overflow-hidden">
-          <div className="p-2 sm:p-4">
-            <iframe
-              src="https://api.leadconnectorhq.com/widget/form/ytF9MgH3xDmSVPGeKAyS"
-              style={{
-                width: '100%',
-                border: 'none',
-                minHeight: '1000px',
-                display: 'block'
-              }}
-              id="inline-ytF9MgH3xDmSVPGeKAyS"
-              data-layout="{'id':'INLINE'}"
-              data-trigger-type="alwaysShow"
-              data-trigger-value=""
-              data-activation-type="alwaysActivated"
-              data-activation-value=""
-              data-deactivation-type="neverDeactivate"
-              data-deactivation-value=""
-              data-form-name="FORMULARIO LANDING PAGE"
-              data-height="958"
-              data-layout-iframe-id="inline-ytF9MgH3xDmSVPGeKAyS"
-              data-form-id="ytF9MgH3xDmSVPGeKAyS"
-              title="FORMULARIO LANDING PAGE"
-            />
-          </div>
+          <iframe
+            src="https://api.leadconnectorhq.com/widget/form/ytF9MgH3xDmSVPGeKAyS"
+            style={{
+              width: '100%',
+              border: 'none',
+              minHeight: '1000px',
+              display: 'block'
+            }}
+            id="inline-ytF9MgH3xDmSVPGeKAyS"
+            data-layout="{'id':'INLINE'}"
+            data-trigger-type="alwaysShow"
+            data-trigger-value=""
+            data-activation-type="alwaysActivated"
+            data-activation-value=""
+            data-deactivation-type="neverDeactivate"
+            data-deactivation-value=""
+            data-form-name="FORMULARIO LANDING PAGE"
+            data-height="958"
+            data-layout-iframe-id="inline-ytF9MgH3xDmSVPGeKAyS"
+            data-form-id="ytF9MgH3xDmSVPGeKAyS"
+            title="FORMULARIO LANDING PAGE"
+          />
         </div>
       </div>
     </section>
